d75d5da3fe292fc1e5c4761e27bc7bc95d9c5f24
imnotdev25 <admin@opurl.co>
2024-01-20 18:59:45 +0530

    ocr

--- bot/plugins/users/misc.py
+++ bot/plugins/users/misc.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from bot.helpers.pricehistory import get_price_history_text
+from bot.helpers.pricehistory import get_price_history, get_price_history_text
 
 
 @Client.on_message(filters.command(["price", "history"]))
@@ -12,18 +12,13 @@ async def pricehistory(_, message: Message):
     replied_message = message.reply_to_message
     try:
         if len(message.command) > 1:
-            content = replied_message.text.split(None, 1)[1]
+            content = message.text.split(None, 1)[1]
 
-        elif replied_message:
-            if replied_message.text:
-                content = replied_message.text
-            else:
-                return await prehistory_reply.edit(pricehistory_usage)
         elif len(message.command) < 2:
             return await prehistory_reply.edit(pricehistory_usage)
 
-        output = await get_price_history_text(content)
-        return await message.reply_text(output) and await prehistory_reply.delete()
-
+        output = await get_price_history(content)
+        caption = await get_price_history_text(content)
+        return await message.reply_photo(output, caption=caption) and await prehistory_reply.delete()
     except Exception as e:
         return await prehistory_reply.edit(f"Something went wrong while getting history. Error: {e}")
