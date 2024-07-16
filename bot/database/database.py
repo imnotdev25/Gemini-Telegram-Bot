0bbe05b59484fc66ce526a3f2bb48b69ea0e6411
imnotdev25 <85677268+imnotdev25@users.noreply.github.com>
2024-07-16 22:15:03 +0530

    Change log: - Removed unused wrappers - poetry environment

--- bot/database/database.py
+++ bot/database/database.py
@@ -19,7 +19,10 @@ async def saveChat(chatid):
     Save the new chat id in the database if it is not already there.
     """
 
-    insert_format = {"date": datetime.now(timezone.utc)}
+    insert_format = {"date": datetime.now(timezone.utc),
+                     "name": chatid.title,
+                     "users": chatid.get_members_count()
+                     }
     await db.chats.update_document(chatid, insert_format)
 
 
